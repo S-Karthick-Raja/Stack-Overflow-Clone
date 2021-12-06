2da9ddd32aff208d8ea9c578bf8ab89c81dffe34
S-Karthick-Raja <kraja2796@gmail.com>
2021-12-06 20:08:45 +0530

    Bottom-Navbar Completed

--- src/Bottom_navbar.js
+++ src/Bottom_navbar.js
@@ -1,7 +1,58 @@
 export function Bottom_navbar() {
   return (
     <div className="Bottom_navbar">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png" style={{ width: "60px" }} alt="stack-overflow-logo" />
+      <div className="logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png"
+          style={{ width: "37px", height: "37px" }}
+          alt="stack-overflow-logo"
+        />
+        <p>Blog</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>LinkedIn</p>
+        <p>Instagram</p>
+      </div>
+      <div>
+        <h5>STACK OVERFLOW</h5>
+        <p>Questions</p>
+        <p>Jobs</p>
+        <p>Developer Jobs Directory</p>
+        <p>Salary Calculator</p>
+        <p>Help</p>
+        <p>Mobile</p>
+        <p>Disable Responsiveness</p>
+      </div>
+      <div>
+        <h5>PRODUCTS</h5>
+        <p>Teams</p>
+        <p>Talent</p>
+        <p>Advertising</p>
+        <p>Enterprise</p>
+      </div>
+      <div>
+        <h5>COMPANY</h5>
+        <p>About</p>
+        <p>Press</p>
+        <p>Work Here</p>
+        <p>Legal</p>
+        <p>Privacy Policy</p>
+        <p>Tearms of service</p>
+        <p>Contact Us</p>
+        <p>Cookie Settings</p>
+        <p>Cookie Policy</p>
+      </div>
+      <div>
+        <h5>STACK EXCHANGE NETWORK</h5>
+        <p>Technology</p>
+        <p>Culture & recreation</p>
+        <p>Life & arts</p>
+        <p>Science</p>
+        <p>Professional</p>
+        <p>Business</p>
+        <p>API</p>
+        <p>Data</p>
+      </div>
     </div>
   );
 }
