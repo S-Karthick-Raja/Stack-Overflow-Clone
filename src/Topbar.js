9b9ad61014d1addf4ab874d82e0674a83c4a0c6d
S-Karthick-Raja <kraja2796@gmail.com>
2021-12-06 20:19:34 +0530

    Login Button Added

--- src/Topbar.js
+++ src/Topbar.js
@@ -4,11 +4,12 @@ import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import HelpIcon from "@mui/icons-material/Help";
 import ViewListIcon from "@mui/icons-material/ViewList";
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LoginIcon from '@mui/icons-material/Login';
 
 export function Topbar() {
   return (
     <div className="Topbar">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/1280px-Stack_Overflow_logo.svg.png"
+        <img className="Stackflow-Home" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/1280px-Stack_Overflow_logo.svg.png"
           alt="Stack-Overflow-LOGO" style={{ width: "120px", heigth: "50px", objectFit: "cover" }} />
         <Button variant="contained">Product</Button>
         <input placeholder="Search…" inputProps={{ ariaLabel: "search" }} style={{ width: "650px", height: "35px" }} />
@@ -17,6 +18,7 @@ export function Topbar() {
         <Button > <EmojiEventsIcon /> </Button>
         <Button > <HelpIcon /> </Button>
         <Button > <ViewListIcon /> </Button>  
+        <Button > <LoginIcon /> </Button>  
     </div>
   );
 }
